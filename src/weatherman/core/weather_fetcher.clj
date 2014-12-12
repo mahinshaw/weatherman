d57a8d606ac5a4fb50951cd260a538a8d90f2ed6
mahinshaw <mahinshaw@gmail.com>
2014-12-12 14:48:09 -0500

    Added search req function and made notes about json response.

--- src/weatherman/core/weather_fetcher.clj
+++ src/weatherman/core/weather_fetcher.clj
@@ -3,31 +3,39 @@
             [org.httpkit.client :as http]
             [cheshire.core :refer :all]))
 
-(def wwo-api-local "http://api.worldweatheronline.com/free/v2/weather.ashx")
-(def wwo-api-search "http://api.worldweatheronline.com/free/v2/search.ashx")
+(def wwo-api "http://api.worldweatheronline.com/free/v2")
+(def wwo-api-local (str/join "/" (cons wwo-api '("weather.ashx"))))
+(def wwo-api-search (str/join "/" (cons wwo-api '("search.ashx"))))
 
 (def api-key (str/trim (slurp "config/wwo_api_key")))
 
-(def req (http/request {:url wwo-api-local
-                        :method :get
-                        :query-params {:key api-key
-                                       :q "Greensboro NC"
-                                       :num_of_days "1"
-                                       :format "json"}}))
+;; The local weather request starts with the "data" key (:data)
+;; Below :data are => :current_condition :request :weather
+(defn local-req
+  "Take a location, and a number of days, to create a request"
+  ([location] (local-req location "1"))
+  ([location days]
+   (http/request {:url wwo-api-local
+                  :method :get
+                  :query-params {:key api-key
+                                 :q location
+                                 :num_of_days (str days)
+                                 :format "json"}})))
 
-(let [resp req]
-  (println "Status: " (:status @resp))
-  (println "Body: " (:body @resp)))
-
-(defn local-req [location days]
-  (http/request {:url wwo-api-local
-                 :method :get
-                 :query-params {:key api-key
-                                :q location
-                                :num_of_days days
-                                :format "json"}}))
+(defn search-req
+  "Use search api to look for a specific location."
+  ([location] (search-req location true))
+  ([location time-zone?] (search-req location time-zone? 10))
+  ([location time-zone? num-results]
+   (http/request {:url wwo-api-search
+                  :method :get
+                  :query-params {:key api-key
+                                 :q location
+                                 :timezone (if time-zone? "yes" "no")
+                                 :num_of_results num-results
+                                 :format "json"}})))
 
 (defn get-resp [request]
   "Take a request and return the body as a hash-map"
-  (let [resp request]
+  (when-let [resp request]
     (parse-string (:body @resp) true)))
